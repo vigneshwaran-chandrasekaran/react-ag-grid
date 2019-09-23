f70c940dc7fb95d8dd67c96170b263fb572a4411
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-09-23 15:08:28 +0530

    live api added, row animation added

--- src/App.js
+++ src/App.js
@@ -1,41 +1,68 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-blue.css';
 
+
 function App() {
+
+  const [users, setUsers] = useState([]);
 
   const gridData = {
     columnDefs: [{
-      headerName: "Make", field: "make", sortable: true, filter: true
+      headerName: "ID", field: "id", sortable: true, filter: true, resizable: true
     }, {
-      headerName: "Model", field: "model", sortable: true, filter: true
+      headerName: "Name", field: "name", sortable: true, filter: true, resizable: true
     }, {
-      headerName: "Price amount", field: "price", sortable: true, filter: true
-    }],
-    rowData: [{
-      make: "Toyota", model: "Celica", price: 35000
+      headerName: "Username", field: "username", sortable: true, filter: true, resizable: true
     }, {
-      make: "Ford", model: "Mondeo", price: 32000
+      headerName: "Email", field: "email", sortable: true, filter: true, resizable: true
     }, {
-      make: "Porsche", model: "Boxter", price: 72000
+      headerName: "Phone", field: "phone", sortable: true, filter: true, resizable: true
+    }, {
+      headerName: "Website", field: "website", sortable: true, filter: true, resizable: true
     }]
   };
 
+  useEffect(() => {
+    getRowData();
+    return () => {
+      console.log(users);
+    };
+  }, []);
+
+  const getRowData = () => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(function (response) {
+        // handle success
+        setUsers(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+        console.log('finally');
+      });
+  };
+
   return (
-    <div className="App">
+    <div>
       <h1>React ag grid</h1>
       <div
         className="ag-theme-blue"
         style={{
           height: '500px',
-          width: '600px'
+          width: 'auto'
         }}
       >
         <AgGridReact
           columnDefs={gridData.columnDefs}
-          rowData={gridData.rowData}>
+          rowData={users}
+          animateRows
+        >
         </AgGridReact>
       </div>
     </div>
