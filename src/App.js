715983ddfa6d5c1bae03a9290645908469b343e9
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-09-23 12:57:14 +0530

    basic react ag-grid component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-blue.css';
 
 function App() {
+
+  const gridData = {
+    columnDefs: [{
+      headerName: "Make", field: "make"
+    }, {
+      headerName: "Model", field: "model"
+    }, {
+      headerName: "Price amount", field: "price"
+    }],
+    rowData: [{
+      make: "Toyota", model: "Celica", price: 35000
+    }, {
+      make: "Ford", model: "Mondeo", price: 32000
+    }, {
+      make: "Porsche", model: "Boxter", price: 72000
+    }]
+  };
+
   return (
     <div className="App">
       <h1>React ag grid</h1>
+      <div
+        className="ag-theme-blue"
+        style={{
+          height: '500px',
+          width: '600px'
+        }}
+      >
+        <AgGridReact
+          columnDefs={gridData.columnDefs}
+          rowData={gridData.rowData}>
+        </AgGridReact>
+      </div>
     </div>
   );
 }
