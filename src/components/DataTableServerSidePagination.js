56ca537567d2c1004d902b83d9cfa64748677380
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-01-27 20:52:48 +0530

    server side pagination done

--- src/components/DataTableServerSidePagination.js
+++ src/components/DataTableServerSidePagination.js
@@ -3,40 +3,63 @@ import React, { useRef, useState, useEffect, useLayoutEffect } from 'react';
 export default function DataTableServerSidePagination() {
 	const dataTable = useRef();
 	const $ = window.jQuery;
-
+	let dt;
 	useLayoutEffect(() => {
 		const $elmt = $(dataTable.current);
-		const dt = $elmt.DataTable({
+		dt = $elmt.DataTable({
 			dom: 'Bfrtip',
 			processing: true,
 			serverSide: true,
 			paging: true,
 			pageLength: 5,
 			deferRender: true,
-			ajax: function(data, callback, settings) {
-				$.ajax({
-					url: 'https://reqres.in/api/users',
-					type: 'get',
-					data: {
-						RecordsStart: data.page,
-						PageSize: data.total_pages,
-					},
-					success: function(data, textStatus, jQxhr) {
-						// https://stackoverflow.com/questions/48867940/how-to-implement-pagination-with-serverside-get-api-in-datatables
-						callback({
-							data: data.data,
-							recordsTotal: data.total,
-							recordsFiltered: data.per_page,
-						});
-						console.log('data', data);
-					},
-					error: function(jqXhr, textStatus, errorThrown) {
-						console.log('jqXhr', jqXhr);
-						console.log('textStatus', textStatus);
-						console.log('errorThrown', errorThrown);
-					},
-				});
+
+			ajax: {
+				url: 'https://reqres.in/api/users',
+				data: function(d) {
+					// https://datatables.net/reference/option/ajax.data
+					console.log('dddd', d);
+					d.page = getPageNumber();
+
+					// newData.page = parseInt(json.start + 1);
+					// console.log('json', newData);
+				},
+				dataFilter: function(data) {
+					var json = $.parseJSON(data);
+					var newData = {};
+					newData.recordsTotal = json.total;
+					newData.recordsFiltered = json.per_page;
+					newData.data = json.data;
+					// newData.page = json.page;
+					console.log('json', newData);
+					return JSON.stringify(newData); // return JSON string
+				},
 			},
+
+			// ajax: function(data, callback, settings) {
+			// 	$.ajax({
+			// 		url: 'https://reqres.in/api/users',
+			// 		type: 'get',
+			// 		data: {
+			// 			RecordsStart: data.page,
+			// 			PageSize: data.total_pages,
+			// 		},
+			// 		success: function(data, textStatus, jQxhr) {
+			// 			// https://stackoverflow.com/questions/48867940/how-to-implement-pagination-with-serverside-get-api-in-datatables
+			// 			callback({
+			// 				data: data.data,
+			// 				recordsTotal: data.total,
+			// 				recordsFiltered: data.per_page,
+			// 			});
+			// 			console.log('data', data);
+			// 		},
+			// 		error: function(jqXhr, textStatus, errorThrown) {
+			// 			console.log('jqXhr', jqXhr);
+			// 			console.log('textStatus', textStatus);
+			// 			console.log('errorThrown', errorThrown);
+			// 		},
+			// 	});
+			// },
 
 			columns: [
 				{
@@ -94,11 +117,31 @@ export default function DataTableServerSidePagination() {
 			});
 		});
 
+		// for column filtering
+		// https://datatables.net/extensions/fixedheader/examples/options/columnFiltering.html
+
 		return () => {
 			// cleanup
-			$elmt.destroy();
+			// $elmt.destroy();
 		};
 	}, []);
+
+	function getPageNumber() {
+		let currentPageIndex = 0;
+		//Is datatable already initialized?
+		if ($.fn.DataTable.isDataTable(dt)) {
+			var info = dt.page.info();
+
+			console.log('info', info);
+			currentPageIndex = info.page;
+		} else {
+			console.log('not init');
+		}
+
+		console.log('currentPageIndex', currentPageIndex);
+
+		return parseInt(currentPageIndex + 1);
+	}
 
 	function format(d) {
 		return (
